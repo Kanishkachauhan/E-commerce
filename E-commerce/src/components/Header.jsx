a72e97e83a95052375b52553bc63181a6fcde0a7
Kanishkachauhan <kanishkachauhan2472006@gmail.com>
2026-05-03 12:05:46 +0530

    routing

---
+++ E-commerce/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css'
+import { NavLink } from 'react-router-dom'
+
+
+
+
+function Header() {
+  return (
+    
+        <header className='header'>
+              
+        
+             
+            <div className='header-div'>
+            <h1>My Store</h1>
+            <nav>
+            <ul className='nav-links'>
+                <li><NavLink to="/">Home</NavLink></li>
+               
+                <li><NavLink to="/login">Login</NavLink></li>
+            </ul>
+            </nav>
+            </div>
+        </header>
+      
+    
+  )
+}
+
+export default Header
